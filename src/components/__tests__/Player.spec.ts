28e769e5b1f83f6b34852a3f6fa2228e088d6529
luis f sousa <rumpertumpski@gmail.com>
2023-09-27 23:14:01 -0300

    add tests to player component

--- src/components/__tests__/Player.spec.ts
+++ src/components/__tests__/Player.spec.ts
@@ -16,4 +16,44 @@ describe('Player', () => {
   it('renders properly', () => {
     expect(wrapper.vm).toBeTruthy()
   })
+
+  it('moves up when the direction is applied', () => {
+    wrapper.vm.state.x = 32
+    wrapper.vm.state.y = 32
+
+    wrapper.vm.move({ x: 0, y: -1 })
+
+    expect(wrapper.vm.state.x).toEqual(32)
+    expect(wrapper.vm.state.y).toEqual(0)
+  })
+
+  it('moves right when the direction is applied', () => {
+    expect(wrapper.vm.state.x).toEqual(0)
+    expect(wrapper.vm.state.y).toEqual(0)
+
+    wrapper.vm.move({ x: 1, y: 0 })
+
+    expect(wrapper.vm.state.x).toEqual(32)
+    expect(wrapper.vm.state.y).toEqual(0)
+  })
+
+  it('moves down when the direction is applied', () => {
+    expect(wrapper.vm.state.x).toEqual(0)
+    expect(wrapper.vm.state.y).toEqual(0)
+
+    wrapper.vm.move({ x: 0, y: 1 })
+
+    expect(wrapper.vm.state.x).toEqual(0)
+    expect(wrapper.vm.state.y).toEqual(32)
+  })
+
+  it('moves left when the direction is applied', () => {
+    wrapper.vm.state.x = 32
+    wrapper.vm.state.y = 32
+
+    wrapper.vm.move({ x: -1, y: 0 })
+
+    expect(wrapper.vm.state.x).toEqual(0)
+    expect(wrapper.vm.state.y).toEqual(32)
+  })
 })

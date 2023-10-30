f6378e0d519905abebca22b7ca14cc11e572d053
luis f sousa <rumpertumpski@gmail.com>
2023-10-29 21:14:10 -0300

    update cell game size

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const IS_SMALL_SCREEN = document.body.clientWidth <= 640
-export const CELL_SIZE = IS_SMALL_SCREEN ? 16 : 24
+export const CELL_SIZE = IS_SMALL_SCREEN ? 12 : 24
 export const GAME_SIZE = {
   width: 16,
   height: 25
